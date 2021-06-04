c19352110bb1c98f471b997592d995b1b529f4f5
Julia <mukhammadieva.julia@gmail.com>
2021-06-04 20:53:13 +0200

    First commit.

---
+++ Monitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monitor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
